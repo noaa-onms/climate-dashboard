34a4a245691fe53c62a07080a0f112ce3986e301
bbest <ben@ecoquants.com>
2024-08-27 10:58:06 +0000

    test git config credential.helper store

--- scripts/update_dashboard.sh
+++ scripts/update_dashboard.sh
@@ -1,15 +1,26 @@
+# setup
 # https://stackoverflow.com/questions/37458287/how-to-run-a-cron-job-inside-a-docker-container
 #   sudo su -
 #   apt-get update && apt-get -y install cron
 #   ln -s /share/github/noaa-onms/climate-dashboard/scripts/update_dashboard.crontab /etc/cron.d/update_dashboard.crontab
 #   chmod 0644 /etc/cron.d/update_dashboard.crontab
 #   rm /etc/cron.d/update_dashboard.crontab
-#   CRON_REPO=/share/github/noaa-onms/climate-dashboard/scripts/update_dashboard.crontab
+#   DIR_REPO=/share/github/noaa-onms/climate-dashboard
+#   CRON_REPO=$DIR_REPO/scripts/update_dashboard.crontab
 #   CRON_SYS=/etc/cron.d/update_dashboard
 #   CRON_LOG=/var/log/cron.log
 #   ln -s $CRON_REPO $CRON_SYS
 #   chmod 0644 $CRON_SYS
 #   touch $CRON_LOG
+# store git credentials
+# https://stackoverflow.com/questions/56057292/git-push-using-crontab-every-hour-prompting-password
+#   cd $DIR_REPO
+#   git config credential.helper store
+$ git push http://example.com/repo.git
+Username: <type your username>
+Password: <type your password>
+
+# git config credential.helper store
 # check
 #   crontab -l
 #   service cron status
